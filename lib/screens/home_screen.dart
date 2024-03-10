07b405147c82bcc948fac7661dc46603fb310b4d
ahmdirfnz <irfanzorro6985@gmail.com>
2024-03-10 14:13:15 +0800

    make the item is visible to all user

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rumah_sewa_app/screens/bill_screen.dart';
+import 'package:rumah_sewa_app/screens/personalTodoList_screen.dart';
 import '../utils/item.dart';
 
-late User loggedinUser;
+User? loggedinUser;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -11,6 +13,163 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final _auth = FirebaseAuth.instance;
+
+  late String _accountName = "";
+  late String _accountEmail = "";
+  late String _accountPictureURL = "";
+
+  Future<void> getCurrentUser() async {
+    try {
+      // await _auth.relo
+      final user = _auth.currentUser;
+      user?.reload();
+      if (user != null) {
+        setState(() {
+          loggedinUser = user;
+          _accountName =
+              user.displayName ?? 'test'; // Using displayName if available
+          _accountEmail = user.email ?? '';
+          _accountPictureURL = user.photoURL ?? 'test';
+          print(_accountName);
+          print(_accountEmail);
+          print(_accountPictureURL);
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void initState() {
+    super.initState();
+    getCurrentUser();
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static List<Widget> _widgetOptions = <Widget>[
+    HomePage(),
+    BillScreen(),
+    TodoList(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  _auth.signOut();
+                  Navigator.pushNamed(context, 'welcome_screen');
+                },
+                icon: Icon(Icons.logout_rounded))
+          ],
+          title: const Text(
+            'Rumah Sewa Management App',
+            style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold),
+          ),
+          backgroundColor: Colors.lightBlueAccent,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text(
+                  _accountName,
+                ),
+                accountEmail: Row(
+                  children: [
+                    Text(
+                      _accountEmail,
+                    ),
+                  ],
+                ),
+                currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage("${_accountPictureURL}")),
+                decoration: BoxDecoration(color: Colors.green),
+                onDetailsPressed: () {
+                  // ...
+                },
+              ),
+              ListTile(
+                title: const Text('Profile'),
+                selected: _selectedIndex == 0,
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(0);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('Settings'),
+                selected: _selectedIndex == 1,
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(1);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: const Text('About'),
+                selected: _selectedIndex == 2,
+                onTap: () {
+                  // Update the state of the app
+                  _onItemTapped(2);
+                  // Then close the drawer
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.note),
+              label: 'Bill',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.shop),
+              label: 'To do list',
+            )
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.amber[800],
+          onTap: _onItemTapped,
+        ),
+        body: Center(
+          child: _widgetOptions.elementAt(_selectedIndex),
+        ));
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final _auth = FirebaseAuth.instance;
   final TextEditingController _itemNameController = TextEditingController();
 
@@ -21,31 +180,25 @@ class _HomeScreenState extends State<HomeScreen> {
 
   Future<void> addItemToFirestore(String category, Item item) async {
     try {
-      await FirebaseFirestore.instance
-          .collection('users')
-          .doc(loggedinUser.uid)
-          .collection(category)
-          .add(item.toMap());
+      await FirebaseFirestore.instance.collection('items').add(item.toMap());
     } catch (e) {
       print('Error adding item: $e');
     }
   }
 
-  Future<List<Item>> fetchItemsFromFirestore(String category) async {
-    List<Item> items = [];
+  Stream<List<Item>> fetchItemsFromFirestore(String category) {
     try {
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-          .collection('users')
-          .doc(loggedinUser.uid)
-          .collection(category)
-          .get();
-      querySnapshot.docs.forEach((doc) {
-        items.add(Item(name: doc['name'], category: doc['category']));
-      });
+      return FirebaseFirestore.instance.collection('items').snapshots().map(
+          (snapshot) => snapshot.docs
+              .map((doc) => Item(
+                  name: doc['name'],
+                  category: doc['category'],
+                  timestamp: doc['timestamp']))
+              .toList());
     } catch (e) {
       print('Error fetching items: $e');
+      return Stream.empty();
     }
-    return items;
   }
 
   //using this function you can use the credentials of the user
@@ -60,114 +213,10 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Bills',
-      style: optionStyle,
-    ),
-    Text(
-      'To do list',
-      style: optionStyle,
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Rumah Sewa App'),
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: Text(
-                'ahmdirfnz',
-              ),
-              accountEmail: const Row(
-                children: [
-                  Text(
-                    'irfanz@gmail.com',
-                  ),
-                ],
-              ),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage:
-                    NetworkImage('https://picsum.photos/250?image=9'),
-              ),
-              decoration: BoxDecoration(color: Colors.green),
-              onDetailsPressed: () {
-                // ...
-              },
-            ),
-            ListTile(
-              title: const Text('Profile'),
-              selected: _selectedIndex == 0,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(0);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Settings'),
-              selected: _selectedIndex == 1,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(1);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('About'),
-              selected: _selectedIndex == 2,
-              onTap: () {
-                // Update the state of the app
-                _onItemTapped(2);
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.note),
-            label: 'Bill',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.shop),
-            label: 'To do list',
-          )
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-      body: Column(
+    return SingleChildScrollView(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           _buildCategoryCard('Barang Masak'),
@@ -191,8 +240,8 @@ class _HomeScreenState extends State<HomeScreen> {
               },
             ),
           ),
-          FutureBuilder<List<Item>>(
-            future: fetchItemsFromFirestore(category),
+          StreamBuilder<List<Item>>(
+            stream: fetchItemsFromFirestore(category),
             builder: (context, snapshot) {
               if (snapshot.connectionState == ConnectionState.waiting) {
                 return CircularProgressIndicator();
@@ -205,8 +254,11 @@ class _HomeScreenState extends State<HomeScreen> {
                     physics: ClampingScrollPhysics(),
                     itemCount: snapshot.data!.length,
                     itemBuilder: (context, index) {
-                      return ListTile(
+                      return CheckboxListTile(
+                        // tileColor: Colors.red,
                         title: Text(snapshot.data![index].name),
+                        value: false,
+                        onChanged: (bool? value) {},
                       );
                     },
                   );
@@ -239,10 +291,13 @@ class _HomeScreenState extends State<HomeScreen> {
             TextButton(
               onPressed: () {
                 if (_itemNameController.text.isNotEmpty) {
+                  final timestamp = Timestamp.now();
                   addItemToFirestore(
-                    category,
-                    Item(name: _itemNameController.text, category: category),
-                  );
+                      category,
+                      Item(
+                          name: _itemNameController.text,
+                          category: category,
+                          timestamp: timestamp));
                   _itemNameController.clear();
                   Navigator.pop(context);
                 }
--- lib/utils/item.dart
+++ lib/utils/item.dart
@@ -3,15 +3,24 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Item {
   String name;
   String category;
+  final Timestamp timestamp; // Add timestamp field
 
-  Item({required this.name, required this.category});
+  Item({
+    required this.name,
+    required this.category,
+    required this.timestamp,
+  });
 
   Map<String, dynamic> toMap() {
-    return {'name': name, 'category': category};
+    return {'name': name, 'category': category, 'timestamp': timestamp};
   }
 
   factory Item.fromSnapshot(DocumentSnapshot snapshot) {
     Map<String, dynamic> data = snapshot.data() as Map<String, dynamic>;
-    return Item(name: data['name'], category: data['category']);
+    return Item(
+      name: data['name'],
+      category: data['category'],
+      timestamp: data['timestamp'],
+    );
   }
 }
